162cc25729cbdca4122a468d72a2d9fa4ff0d61c
RAVELOTAHIANA <9691748+Liivasoa@users.noreply.github.com>
2026-03-30 12:10:46 +0200

    [Conf][All] Init springboot project

---
+++ src/main/java/mg/msys/gutenber_ws/GutenberWsApplication.java
@@ -0,0 +1,13 @@
+package mg.msys.gutenber_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutenberWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutenberWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/msys/gutenber_ws/GutenberWsApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.msys.gutenber_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutenberWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
