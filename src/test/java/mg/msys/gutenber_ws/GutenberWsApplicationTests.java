9474990850fb05639dea54153cb439a1b218efdc
RAVELOTAHIANA <9691748+Liivasoa@users.noreply.github.com>
2026-03-30 14:45:36 +0200

    [Conf][All] Add dependency and configure the app

--- src/test/java/mg/msys/gutenber_ws/GutenberWsApplicationTests.java
+++ src/test/java/mg/msys/gutenber_ws/GutenberWsApplicationTests.java
@@ -2,9 +2,18 @@ package mg.msys.gutenber_ws;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GutenberWsApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:18.1-alpine");
 
 	@Test
 	void contextLoads() {
